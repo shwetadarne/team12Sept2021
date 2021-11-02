b979bcb895ac76717dd8359018dfa274f1eda3d2
shwetadarne <93431201+shwetadarne@users.noreply.github.com>
2021-11-02 18:28:16 +0530

    Add files via upload

---
+++ OOP/Assignment/multibinary.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class multibinary
+{
+   public static void main(String args[])
+   {
+	    Scanner sc = new Scanner(System.in);
+
+	long b1, b2;
+	int i = 0,// carry = 0;
+
+	int[] sum = new int[10];
+
+	System.out.print("Enter 1  binary number: ");
+	b1 = sc.nextLong();
+	System.out.print("Enter second binary number: ");
+	b2 = sc.nextLong();
+
+	while (b1 != 0 || b2 != 0) 
+	{
+		sum[i++] = (int)(b1 % 10 * b2 % 10 );
+		//carry = (int)((b1 % 10 + b2 % 10 + carry) / 2);
+		b1 = b1 / 10;
+		b2 = b2 / 10;
+	}
+	//if (carry != 0) {
+		//sum[i++] = carry;
+	}
+	--i;
+	System.out.print("Output: ");
+	while (i >= 0) 
+	{
+		System.out.print(sum[i--]);
+	}
+	System.out.println("");  
+   }
