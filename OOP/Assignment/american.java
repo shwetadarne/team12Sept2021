f6151efa43349361fa469ace7ba9d4f80c2e5c64
shwetadarne <93431201+shwetadarne@users.noreply.github.com>
2021-11-02 18:23:44 +0530

    Add files via upload

---
+++ OOP/Assignment/american.java
@@ -0,0 +1,32 @@
+class american
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=15;i++)
+		{ 
+	      
+		  
+			  
+			for(int j=1;j<=5;j++)
+			{
+			
+				if(i<10)
+				{
+			
+				System.out.print(" * ");
+				}
+				else
+					System.out.print(" ====");
+			}
+			System.out.println("  ");
+		  }
+			for(int k=1;k<=10;k++)
+			{
+				System.out.print("====");
+			}
+			System.out.println("  ");
+		}
+	}
+
+
+				
