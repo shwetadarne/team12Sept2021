f2ad37e1b1be29fea30fed0d31d68bcb2d12ef65
shwetadarne <93431201+shwetadarne@users.noreply.github.com>
2021-11-02 18:24:08 +0530

    Add files via upload

---
+++ OOP/Assignment/average.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class average
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println(" enter 3 numbers for average");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d = (a+b+c)/3;
+		
+		System.out.println(" average of 3 numbers: "+d);
+	}
+}
+
+		
