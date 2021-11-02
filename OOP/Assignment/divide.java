ccd1e51ba2561fd6717f846e5130e15f36827687
shwetadarne <93431201+shwetadarne@users.noreply.github.com>
2021-11-02 18:26:32 +0530

    Add files via upload

---
+++ OOP/Assignment/divide.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class divide
+{
+	public static void main(String args[])
+
+	{
+       Scanner sc= new Scanner(System.in);
+         System.out.println("enter 2 number for divide");
+         int x=sc.nextInt();
+		 int y=sc.nextInt();
+		 int z=x/y;
+		 System.out.println("division of two number"+z);
+	}
+}
+
+		 
+		 
